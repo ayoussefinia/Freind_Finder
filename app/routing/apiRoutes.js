3c1a4763a9016c2ba7217de08ffe15ed4c8e0212
ayoussefinia <alex.youssefinia@gmail.com>
2019-07-11 00:22:50 -0500

    added functionality for taking the form post request and comparing choices against data to get best match, still working on styles

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,9 @@
 var friendsData = require("../data/friends");
 var fs = require("fs");
 
-  
+//function for calculating the sum of elements in an array
+const arrSum = arr => arr.reduce((a,b) => a + b, 0)
+const arrMin = arr => Math.min(...arr);
   
   module.exports = function(app) {
   
@@ -12,10 +14,49 @@ var fs = require("fs");
 
     // If no matching route is found default to home
     app.post("/api/friends", function(req, res) {
+      var differenceArr = [];
+      var userChoices = req.body.scores;
+      
+      // console.log(userChoices);
+      
+      // for(i=0; i<friendsData.length; i++) {
+      //   var comparison = f
+      //   console.log(friendsData[i].scores);
+      // }
+      for(i=0; i<friendsData.length; i++) {
+        var comparisonArr = [];
+        for(j=0;j<userChoices.length; j++) {
+          var diff = Math.abs(Number(userChoices[j])-Number(friendsData[i].scores[j]));
+          comparisonArr.push(diff);
+        }
+        var totalDiff = arrSum(comparisonArr)
+        // var name = friendsData[i].name;
+        differenceArr.push(totalDiff);
+      }
+
       friendsData.push(req.body);
-    
-      console.log(req.body);
-      res.json(true);
+      var minDiff = arrMin(differenceArr);
+      var diffIndex =  differenceArr.indexOf(minDiff);
+
+
+      var friendName =  friendsData[diffIndex].name;
+      var friendPic =   friendsData[diffIndex].photo;
+
+      // alert(friendPic, friendName);
+      var friendObj = {
+        "name": friendName,
+        "image": friendPic
+      };
+
+      console.log(friendsData.length);
+      console.log("differences Arr: " ,  differenceArr);
+      console.log(diffIndex);
+
+      // console.log("total ", totalDiff);
+      // console.log("comparison Arr:" , comparisonArr);
+      // console.log(friendsData);
+      // console.log(req.body);
+      res.json(friendObj);
     });
 
   }
